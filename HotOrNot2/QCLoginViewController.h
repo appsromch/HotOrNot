d152979f1a92391c3ca4bb2657118bf05aec207c
Eliot Arntz <arntzel@gmail.com>
2013-06-20 17:00:56 -0400

    first commit

---
+++ HotOrNot2/QCLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  QCLoginViewController.h
+//  HotOrNot2
+//
+//  Created by Eliot Arntz on 6/20/13.
+//  Copyright (c) 2013 self.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QCLoginViewController : UIViewController
+
+@end
---
+++ HotOrNot2/QCLoginViewController.m
@@ -0,0 +1,38 @@
+//
+//  QCLoginViewController.m
+//  HotOrNot2
+//
+//  Created by Eliot Arntz on 6/20/13.
+//  Copyright (c) 2013 self.edu. All rights reserved.
+//
+
+#import "QCLoginViewController.h"
+
+@interface QCLoginViewController ()
+
+@end
+
+@implementation QCLoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
