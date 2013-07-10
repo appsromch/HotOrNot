a76af37f07657f1de622f3a7106798715170d98f
arntzel <arntzel@gmail.com>
2013-07-09 20:32:28 -0400

    first commit

---
+++ HotOrNot2/QCCollectionViewFlowLayout.m
@@ -0,0 +1,13 @@
+//
+//  QCCollectionViewFlowLayout.m
+//  HotOrNot2
+//
+//  Created by Eliot Arntz on 7/9/13.
+//  Copyright (c) 2013 self.edu. All rights reserved.
+//
+
+#import "QCCollectionViewFlowLayout.h"
+
+@implementation QCCollectionViewFlowLayout
+
+@end
---
+++ HotOrNot2/QCMatchingMakingViewController.m
@@ -0,0 +1,38 @@
+//
+//  QCMatchingMakingViewController.m
+//  HotOrNot2
+//
+//  Created by Eliot Arntz on 7/9/13.
+//  Copyright (c) 2013 self.edu. All rights reserved.
+//
+
+#import "QCMatchingMakingViewController.h"
+
+@interface QCMatchingMakingViewController ()
+
+@end
+
+@implementation QCMatchingMakingViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
