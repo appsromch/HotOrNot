c745eaa93993bf6a5a71a66f5f21b1245a4faf46
wshi27 <wshi27@hotmail.com>
2013-07-15 23:23:28 -0400

    first commit

---
+++ HotOrNot2/QCLikerAndDisliker.h
@@ -0,0 +1,13 @@
+//
+//  QCLikerAndDisliker.h
+//  HotOrNot
+//
+//  Created by Weiwei Shi on 7/15/13.
+//  Copyright (c) 2013 self.edu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QCLikerAndDisliker : NSObject
+
+@end
---
+++ HotOrNot2/QCLikerAndDisliker.m
@@ -0,0 +1,13 @@
+//
+//  QCLikerAndDisliker.m
+//  HotOrNot
+//
+//  Created by Weiwei Shi on 7/15/13.
+//  Copyright (c) 2013 self.edu. All rights reserved.
+//
+
+#import "QCLikerAndDisliker.h"
+
+@implementation QCLikerAndDisliker
+
+@end
