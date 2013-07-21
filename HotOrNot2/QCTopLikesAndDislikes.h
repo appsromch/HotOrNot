927a984fd5a61a29686f12b332eef57a3cb1d824
wshi27 <wshi27@hotmail.com>
2013-07-21 11:56:40 -0400

    leaderBoard bug

---
+++ HotOrNot2/QCTopLikesAndDislikes.h
@@ -0,0 +1,13 @@
+//
+//  QCTopLikesAndDislikes.h
+//  HotOrNot
+//
+//  Created by Weiwei Shi on 7/21/13.
+//  Copyright (c) 2013 self.edu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QCTopLikesAndDislikes : NSObject
+
+@end
---
+++ HotOrNot2/QCTopLikesAndDislikes.m
@@ -0,0 +1,13 @@
+//
+//  QCTopLikesAndDislikes.m
+//  HotOrNot
+//
+//  Created by Weiwei Shi on 7/21/13.
+//  Copyright (c) 2013 self.edu. All rights reserved.
+//
+
+#import "QCTopLikesAndDislikes.h"
+
+@implementation QCTopLikesAndDislikes
+
+@end
